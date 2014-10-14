0d51089ae12a04d9a28821e0ac354a741b6e6e77
gofd <gofd@medieninf.de>
2014-10-14 21:43:53 +0200

    gofmt, emacs problem

--- propagator/interval/C1XmultC2YeqC3ZBounds.go
+++ propagator/interval/C1XmultC2YeqC3ZBounds.go
@@ -223,7 +223,7 @@ func (this *C1XmultC2YeqC3ZBounds) Register(store *core.Store) {
 	var domains map[core.VarId]core.Domain
 	this.inCh, domains, this.outCh =
 		store.RegisterPropagatorMap([]core.VarId{this.x, this.y, this.z},
-		this.id)
+			this.id)
 	this.x_Domain = core.GetVaridToIntervalDomain(domains[this.x])
 	this.y_Domain = core.GetVaridToIntervalDomain(domains[this.y])
 	this.z_Domain = core.GetVaridToIntervalDomain(domains[this.z])
