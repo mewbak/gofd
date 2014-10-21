7cb260c952c9ff227d6b2ad07534372c004d69b7
gofd <gofd@medieninf.de>
2014-10-21 18:11:45 +0200

    update run-benchs.sh, flexible selection of benchmark to run

--- bench/run-benchs.sh
+++ bench/run-benchs.sh
@@ -1,74 +1,84 @@
-#!/bin/sh
+#!/bin/bash
+# run benchmark programs
+# run from within the directory with ./run-benchs.sh
+# Benchmark programs have the filename bench_<name>.go
+# and are in the main package. The only other package 
+# the need for compilation is benchframework.go (of course
+# anything else can be imported and used). 
+# For each run all benchmark programs are compiled to an 
+# executable under BIN_FOLD and executed. 
+# It is possible to restrict the compiled benchmark programs to the
+# ones that contain the first command line parameter.
+# It is possible to restrict the executed benchmark functions to the 
+# ones whose name contains the second command line parameter. 
+# Try to choose names that can easily be selected.
+
+# Examples: 
+# run all benchmarks
+# $ ./run-benchs.sh 
+# only run the benchmarks in bench_IntVar.go
+# $ ./run-benchs.sh IntVar
+# only run the benchmarks in bench_IvDomain.go and 
+# of these only the Bad.ExD ones
+# clean first as well
+# $ ./run-benchs.sh -c IvDomain Bad.ExD
+
 # We need to set manually the GOPATH, which is a couple of directories
 # above starting from bench; what a hack....
 export GOPATH=$(dirname $(dirname $(dirname $(dirname $(dirname $(pwd))))))
 # now we may start go with a main and it will find the other packages
 
-set_local_test_environment(){
-  echo "parameters for benching local environment"
-  GO=go
-  LOG_FOLD=logs
-  BIN_FOLD=bin
-}
+# configuration for benching
+GOCMD=go
+LOG_FOLD=logs
+BIN_FOLD=bin
 
-set_compute_environment(){
-  echo "parameters for benching compute environment"
-  GO=go
-  LOG_FOLD=logs
-  BIN_FOLD=bin
-}
+# ensure that the directories exist
+mkdir -p $LOG_FOLD
+mkdir -p $BIN_FOLD
 
-clean(){
+# optional, realclean bin and log
+clean() {	
+	echo "CLEANING"
   /bin/rm -rf $LOG_FOLD
   /bin/rm -rf $BIN_FOLD
   mkdir $LOG_FOLD
   mkdir $BIN_FOLD
 }
 
-# preparation: setting up bench-environment, building executables
-build() {  
-  #-- basic --
-  $GO build -o ./$BIN_FOLD/bench_Domain bench_Domain.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_GC1XeqC2Y bench_GC1XeqC2Y.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_GC1XplusC2YeqC3Z bench_GC1XplusC2YeqC3Z.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_IntVar bench_IntVar.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_Simple bench_Simple.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_Store bench_Store.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_XmultCeqY bench_XmultCeqY.go benchframework.go
-  #-- intervals --
-  $GO build -o ./$BIN_FOLD/bench_IvDomain bench_IvDomain.go benchframework.go
-  #-- applications --
-  $GO build -o ./$BIN_FOLD/bench_CarSequencing bench_CarSequencing.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_MagicSeries bench_MagicSeries.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_MysteryShopper bench_MysteryShopper.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_NQueens bench_NQueens.go benchframework.go
-  $GO build -o ./$BIN_FOLD/bench_SMM bench_SMM.go benchframework.go
+function build_bench {
+	$GOCMD build -o ./$BIN_FOLD/bench_$1 benchframework.go bench_$1.go
+}
+
+# warm up, intention is to prevent the processor to be in deep sleepstates
+function warmup {
+	$GOCMD run warmup.go 
+}
+
+function run_bench {
+	build_bench $1
+    warmup
+    ./$BIN_FOLD/bench_$1 "${*:2}"
 }
 
 
-#- bench execution (function calls) -
-set_local_test_environment
-#set_compute_environment
+# go through options, accept -c and -g <gocmd>
+while getopts "cg:" arg; do
+case "$arg" in
+        g) GOCMD="$OPTARG";;
+        c) GOCLEAN="cleaning bin and logs first"; clean;;
+        [?]) echo >&2 "Usage: $0 [-g gocmd (go)] [-c] [packages]"
+         echo >&2 "   add -c to clean bin and log first"
+            exit 1;;
+    esac
+done
+shift $(($OPTIND-1))
 
-clean
-build
-
-#- warm up -
-echo "warm up"
-$GO run warmup.go
-
-#- benching-
-./$BIN_FOLD/bench_Domain
-./$BIN_FOLD/bench_GC1XeqC2Y
-./$BIN_FOLD/bench_GC1XplusC2YeqC3Z
-./$BIN_FOLD/bench_IntVar
-./$BIN_FOLD/bench_Simple
-./$BIN_FOLD/bench_Store
-./$BIN_FOLD/bench_XmultCeqY
-./$BIN_FOLD/bench_IvDomain
-./$BIN_FOLD/bench_CarSequencing
-./$BIN_FOLD/bench_MagicSeries
-./$BIN_FOLD/bench_MysteryShopper
-./$BIN_FOLD/bench_NQueens
-./$BIN_FOLD/bench_SMM
-
+echo "Benching with : $GOCMD    "$GOCLEAN
+ls bench_*.go | while read f; do 
+BENCH=$(echo $f | sed -r 's/(^bench_|\.go$)//g')
+if [[ $f == *$1* ]]; then # empty $1 automagically fits
+echo "=== Benching $BENCH ==="
+run_bench $BENCH "${*:2}"
+fi
+done
