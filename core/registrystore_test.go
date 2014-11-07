ffb15ea14f45e81febfa30cc4c0ffdfdf16e5c64
mduchmann <mduchmann@localhost>
2014-11-07 09:49:06 +0100

    - registrystore: one more test for "RemoveFixedRelations"

--- core/registrystore_test.go
+++ core/registrystore_test.go
@@ -49,11 +49,10 @@ func Test_GetSetHasName(t *testing.T) {
 	}
 }
 
-// TODO: more tests like this (more constraints, more variables)
 func Test_RemoveFixedRelations0(t *testing.T) {
 	setup()
 	defer teardown()
-	log("RegistryStoreRemoveFixedRelations")
+	log("RegistryStoreRemoveFixedRelations0")
 
 	rs := CreateRegistryStore()
 	// setup... X=0,Y=1,Z=2 and constraint X+Y=Z
@@ -71,10 +70,10 @@ func Test_RemoveFixedRelations0(t *testing.T) {
 
 	// now, X will be fixed (then, no constraint should listen on X and
 	// c1 should only listen to Y, Z)
-	rs.RemoveFixedRelations(0)
+	rs.RemoveFixedRelations(0) //supposed, X is fixed
 	m := make(map[*ConstraintData]int)
-	m[constraintData] = 2
-	testRegistrations(t, rs, 1, m, 0)
+	m[constraintData] = 2             // c1 interested in Y,Z anymore (len=2)
+	testRegistrations(t, rs, 1, m, 0) // numberConstraints:1, fixedVar:0
 
 	rs.RemoveFixedRelations(2)
 	m = make(map[*ConstraintData]int)
@@ -85,6 +84,67 @@ func Test_RemoveFixedRelations0(t *testing.T) {
 	m = make(map[*ConstraintData]int)
 	m[constraintData] = 0
 	testRegistrations(t, rs, 0, m, 1)
+}
+
+func Test_RemoveFixedRelations1(t *testing.T) {
+	setup()
+	defer teardown()
+	log("RegistryStoreRemoveFixedRelations1")
+
+	rs := CreateRegistryStore()
+	//setup... with e.g. X=0, Y=1, Z=2, Q=3, P=4
+	//e.g. constraint X+Y=Z
+	varids := []VarId{0, 1, 2}
+	c1, _ := createPropagatorDummy(varids, t)
+	rs.constraints[c1.GetID()] = c1
+	interestedVarids := make([]VarId, len(varids))
+	copy(interestedVarids, varids)
+	writeChannel := make(chan *ChangeEntry, 10)
+	rs.RegisterVarIdWithConstraint(c1.GetID(), writeChannel,
+		varids, interestedVarids)
+
+	//e.g. constraint Y+Z+Q=P
+	varids = []VarId{1, 2, 3, 4}
+	c2, _ := createPropagatorDummy(varids, t)
+	rs.constraints[c2.GetID()] = c2
+	interestedVarids = make([]VarId, len(varids))
+	copy(interestedVarids, varids)
+	writeChannel = make(chan *ChangeEntry, 10)
+	rs.RegisterVarIdWithConstraint(c2.GetID(), writeChannel,
+		varids, interestedVarids)
+
+	constraintData1 := rs.varIdsToConstraints[0][0]
+	constraintData2 := rs.varIdsToConstraints[4][0]
+
+	// now, X will be fixed (then, no constraint should listen on X and
+	// c1 should only listen to Y, Z)
+	rs.RemoveFixedRelations(0)
+	m := make(map[*ConstraintData]int)
+	m[constraintData1] = 2
+	m[constraintData2] = 4
+	testRegistrations(t, rs, 2, m, 0)
+
+	rs.RemoveFixedRelations(2)
+	m = make(map[*ConstraintData]int)
+	m[constraintData1] = 1
+	m[constraintData2] = 3
+	testRegistrations(t, rs, 2, m, 2)
+
+	rs.RemoveFixedRelations(1)
+	m = make(map[*ConstraintData]int)
+	m[constraintData1] = 0
+	m[constraintData2] = 2
+	testRegistrations(t, rs, 1, m, 1)
+
+	rs.RemoveFixedRelations(3)
+	m = make(map[*ConstraintData]int)
+	m[constraintData2] = 1
+	testRegistrations(t, rs, 1, m, 3)
+
+	rs.RemoveFixedRelations(4)
+	m = make(map[*ConstraintData]int)
+	m[constraintData2] = 0
+	testRegistrations(t, rs, 0, m, 4)
 }
 
 func testRegistrations(t *testing.T, rs *RegistryStore,
